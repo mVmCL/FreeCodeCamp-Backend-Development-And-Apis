985e46eb01a0feb8c95c051b5974989a19d4d798
©¤╬¤/\/\\//\/\C|X¤╬¤© <mvmclx@gmail.com>
2023-02-05 23:05:20 -0600

    linking front end

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,8 @@ router.route('/').get(currentTime)
 router.route('/:date').get(convertDateToUnixTime)
 // .post(convertDateToUnixTime)
 // router.get('/notes', getNotes)
-
+router.get('/', (req, res) =>{
+    res.render('./views/index.html',{title:'Home'})
+})
 
 module.exports = router
